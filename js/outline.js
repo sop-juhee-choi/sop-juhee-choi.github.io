b88ed3cc37a401bc886ae2d4d9bd655c83211d1e
Benjamin J. Choi <benchoi@ccs.tsukuba.ac.jp>
2024-12-10 23:33:24 +0900

    fixed bug

--- js/outline.js
+++ js/outline.js
@@ -1,3 +1,8 @@
-document.querySelectorAll('.outlined-text').forEach(function (element) {
-    element.setAttribute('data-text', element.textContent);
+// document.querySelectorAll('.outlined-text').forEach(function (element) {
+//     element.setAttribute('data-text', element.textContent);
+// });
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.outlined-text').forEach(function (element) {
+        element.setAttribute('data-text', element.textContent);
+    });
 });
