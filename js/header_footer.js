f44c9c78216b95a7bc8c6b1ac5ff923120056f53
Benjamin J. Choi <benchoi@ccs.tsukuba.ac.jp>
2026-02-07 19:52:12 +0900

    updated

---
+++ js/header_footer.js
@@ -0,0 +1,24 @@
+function loadHeader() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error loading header:', error);
+        });
+}
+
+function loadExternalHTML(filePath, divId) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(divId).innerHTML = data;
+            if (window.MathJax) {
+                MathJax.typeset();
+            }
+        })
+        .catch(error => {
+            console.error('Error loading external HTML:', error);
+        });
+}
